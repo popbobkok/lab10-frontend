05f49fb55eb2c22e624bcbb57284247d77c2c8ba
Natthapong_Pop <popbobkok34@hotmail.com>
2022-09-22 17:18:04 +0700

    5.2 vue form edit

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,26 @@
+<!-- eslint-disable vue/no-parsing-error -->
+<template>
+  <label v-if="label">{{ label }}</label>
+  <input
+    class="field"
+    :placeholder="label"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    v-bind="$attr"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  }
+}
+</script>
